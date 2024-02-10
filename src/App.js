413ed5a2f0c62534b16f8d81a6710a04519315c7
Vova <podolian@gmail.com>
2024-02-10 22:49:40 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      calc
+      <BillComponent />
+      <SelectPercentage>
+        <span>How did you like the service?</span>
+      </SelectPercentage>
+      <SelectPercentage>
+        <span>How did you friends like the service?</span>
+      </SelectPercentage>
+      <OutPutComponent />
+      <ResetComponent>
+        <span>Reset</span>
+      </ResetComponent>
     </div>
   );
 }
 
 export default App;
+
+function BillComponent() {
+  return (
+    <div>
+      <span>How much was the bill?</span>
+      <input type="text" />
+    </div>
+  );
+}
+
+function SelectPercentage({ children }) {
+  return (
+    <div>
+      {children}
+      <select>
+        <option>Dissatisfied (0%)</option>
+        <option>It was ok (5%)</option>
+        <option>It was good (10%) </option>
+        <option>Absolutely amazing (20%) </option>
+      </select>
+    </div>
+  );
+}
+
+function OutPutComponent() {
+  return (
+    <div>
+      <h3>You pay 92$ (80$ + 12$ tip)</h3>
+    </div>
+  );
+}
+
+function ResetComponent({ children }) {
+  return (
+    <div>
+      <button>{children}</button>
+    </div>
+  );
+}
